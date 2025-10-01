7c8211d1139da04664df4ca7feb5375e73e84521
Christopher Kruger <krugerchristopher99@gmail.com>
2025-09-30 18:03:17 -0700

    feat: Add TypeScript definitions for additional custom JSX elements

---
+++ src/global.d.ts
@@ -0,0 +1,26 @@
+export {}
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'sp-button': any;
+      'sp-button-group': any;
+      'sp-label': any;
+      'sp-textarea': any;
+      'sp-picker': any;
+      'sp-menu': any;
+      'sp-menu-item': any;
+      'sp-radio-group': any;
+      'sp-radio': any;
+      'sp-slider': any;
+      'sp-divider': any;
+      'sp-progressbar': any;
+      'sp-textfield': any;
+      'sp-checkbox': any;
+      'sp-dropdown': any;
+      'sp-icon': any;
+      'sp-link': any;
+      'sp-action-button': any;
+    }
+  }
+}
